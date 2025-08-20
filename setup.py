93db7d8c2e10b8d2e48caf60f3e27a6d499184f2
“prodip <prodip1023@gmail.com>
2025-08-20 22:26:14 +0530

    add requirements

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="prodip",
+    author_email="prodip1023@gmail.com",
+    packages=find_packages()
+)
