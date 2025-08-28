635bc75320a0d358ae9496d381f6ad97243229c7
“prodip <prodip1023@gmail.com>
2025-08-28 23:47:21 +0530

    add component

--- demo.py
+++ demo.py
@@ -1,11 +1,18 @@
-from us_visa.logger import logging
-from us_visa.exception import USvisaException
-import sys
+# from us_visa.logger import logging
+# from us_visa.exception import USvisaException
+# import sys
 
 
-try:
-    r = 3/0
-    print(r)
-except Exception as e:
-    logging.info(e)
-    raise USvisaException(e, sys)
+# try:
+#     r = 3/0
+#     print(r)
+# except Exception as e:
+#     logging.info(e)
+#     raise USvisaException(e, sys)
+
+
+from us_visa.pipline.training_pipeline import TrainingPipeline
+
+
+pipeline = TrainingPipeline()
+pipeline.run_pipeline()
